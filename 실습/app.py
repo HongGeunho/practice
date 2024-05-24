f63c0ebc677a372a8b4d834f6bba8314f42b47b7
HongGeunho <164463769+HongGeunho@users.noreply.github.com>
2024-05-24 17:20:46 +0900

    Add files via upload

---
+++ 실습/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    album = db.Column(db.String(100))
+    filename = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Song {self.title}>'
+
+@app.route('/')
+def index():
+    songs = Song.query.all()
+    return render_template('index.html', songs=songs)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        artist = request.form['artist']
+        album = request.form['album']
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            new_song = Song(title=title, artist=artist, album=album, filename=filename)
+            db.session.add(new_song)
+            db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/play/<filename>')
+def play(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+    
+    if __name__ == '__main__':
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/songs.db'
+    
+    @app.route('/play/<int:song_id>', methods=['GET'])
+    def play(song_id):
+        song = Song.query.get_or_404(song_id)
+        return render_template('play.html', song=song)
